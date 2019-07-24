16d307677d6f12d6c373ebb0af10782aa997ad51
Malya727 <41793331+Malya727@users.noreply.github.com>
2019-07-24 20:25:37 +0530

    Add files via upload

---
+++ day2assign9.py
@@ -0,0 +1,19 @@
+inp = "Comprehensions are a feature of Python which I would really miss if I ever have to leave it.Comprehensions are constructs that allow sequences to be built from other sequences.Several types of comprehensions are supported in both Python2 and Python3"
+
+li = inp.strip(" ").split(" ")
+
+di = {}
+
+for l in li:
+    if not l in di:
+        di[l] = 1
+    else:
+        di[l] += 1
+
+
+ma = max(di.values())
+mi = min(di.values())
+
+for m,n in di.items():
+    if n == ma or n == mi:
+        print (f" {m} : {n}")
